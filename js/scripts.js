18a91a91704637183a567580d42c6782e2d2d412
Shivansh Tiwari <113783549+shivanshrider@users.noreply.github.com>
2024-07-02 04:00:33 +0530

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -62,36 +62,36 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 
-// var mainListDiv = document.getElementById("mainListDiv"),
-//     mediaButton = document.getElementById("mediaButton");
-
-// mediaButton.onclick = function () {
-    
-//     "use strict";
-    
-//     mainListDiv.classList.toggle("show_list");
-//     mediaButton.classList.toggle("active");
-    
-// };
-
-// const navToggle = document.querySelector('.nav-toggle');
-// const navItems = document.querySelector('.nav-items');
-
-// navToggle.addEventListener('click', () => {
-//   navToggle.classList.toggle('active');
-//   navItems.classList.toggle('active');
-// });
-
-// document.addEventListener('DOMContentLoaded', function() {
-//     const mediaButton = document.getElementById('mediaButton');
-//     const mainListDiv = document.getElementById('mainListDiv');
-  
-//     mediaButton.addEventListener('click', function() {
-//       this.classList.toggle('active');
-//       mainListDiv.classList.toggle('active');
-//     });
-//   });
 
   
   
-  
+const slides = document.querySelectorAll("[data-slide]");
+const buttons = document.querySelectorAll("[data-button]");
+
+let currSlide = 0;
+let maxSlide = slides.length - 1;
+
+const updateCarousel = (number = 0) => {
+  slides.forEach((slide, index) => {
+    slide.style.transform = `translateX(${(index - number) * 100}%)`;
+  });
+};
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    button.dataset.button == "next" ? ++currSlide : --currSlide;
+
+    if (currSlide > maxSlide) {
+      currSlide = 0;
+    } else if (currSlide < 0) {
+      currSlide = maxSlide;
+    }
+
+    updateCarousel(currSlide);
+  });
+});
+
+updateCarousel();
+
+
+
